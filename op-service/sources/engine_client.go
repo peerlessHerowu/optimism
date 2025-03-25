b1d2c844f7c7b60ca9ea8bcbcd9827843dbb7ee4
peerheros <3077263001@qq.com>
2025-03-25 15:51:04 +0800

    log

--- op-service/sources/engine_client.go
+++ op-service/sources/engine_client.go
@@ -86,11 +86,13 @@ func (s *EngineAPIClient) ForkchoiceUpdate(ctx context.Context, fc *eth.Forkchoi
 		s2 = "engine_forkchoiceUpdatedV1"
 	}
 	err := s.RPC.CallContext(ctx, &result, s2, fc, attributes)
+	llog.Info("wjd Failed to share forkchoice-updated signal1111", "err", err, "method", method, "s2", s2, "result", result, "s", s)
+
 	if err != nil {
-		llog.Warn("Failed to share forkchoice-updated signal", "err", err, "method", method, "result", result, "s", s)
+		llog.Warn("Failed to share forkchoice-updated signal errr", "err", err, "method", method, "s2", s2, "result", result, "s", s)
 		return nil, err
 	}
-	llog.Info("Failed to share forkchoice-updated signal", "err", err, "method", method, "result", result, "s", s)
+	llog.Info("Failed to share forkchoice-updated signal success", "err", err, "method", method, "result", result, "s", s)
 	if attributes != nil { // block building is optional, we only get a payload ID if we are building a block
 		tlog.Trace("Received payload id", "payloadId", result.PayloadID)
 	}
